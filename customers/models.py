e79dfc6959f6b9ca70857d4811330d1d19865d0f
Abhishek Jain <abhishekjai35@gmail.com>
2025-04-13 19:27:35 +0530

    Add basic customer view

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+  STATUS_CHOICES = [
+    ('lead', 'Lead'),
+    ('converted', 'Converted'),
+    ('lost', 'Lost'),
+  ]
+
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+  status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+  def __str__(self):
+    return f"{self.name} ({self.status})"
